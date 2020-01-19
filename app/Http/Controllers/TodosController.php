b5d03667af416146f08b5c77a5e6b48519a57f8a
Francisco Campos <franciiscocampos170@gmail.com>
2020-01-19 12:34:06 +0100

    fetch all todos from database and display then to the index

--- app/Http/Controllers/TodosController.php
+++ app/Http/Controllers/TodosController.php
@@ -3,11 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Todo;
 
 class TodosController extends Controller
 {
     public function index()
     {
-        return view('todos.index');
+        //fetch all todos  from database
+        //display them in the todos.index page
+        $todos =  Todo::all();
+        return view('todos.index')->with('todos', $todos);
     }
 }
